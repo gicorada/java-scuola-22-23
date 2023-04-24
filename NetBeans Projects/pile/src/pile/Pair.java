007ab406974151ca9260e8e1df2e6be69890d8d6
Giacomo Radaelli <gradaellig@protonmail.com>
2023-04-24 14:20:10 +0200

    Aggiunto pile da finire

---
+++ NetBeans Projects/pile/src/pile/Pair.java
@@ -0,0 +1,29 @@
+package pile;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author giacomo
+ */
+public class Pair {
+    private int row;
+    private int column;
+
+    public Pair(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+}
